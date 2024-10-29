48679017f9f47ec7994920a7db621684bdcbbb21
Sabah <sabahaa@kth.se>
2024-10-29 15:48:44 +0100

    edit sabah branch

--- GitIntro/Program.cs
+++ GitIntro/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, group!");
+            Console.WriteLine("Hello,  from sabah beansh ");
         }
     }
 }
