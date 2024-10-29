caf6e10fa0655488e7e9ac1b91ed987e048dfda8
Sabah <sabahaa@kth.se>
2024-10-29 15:07:44 +0100

    add class

---
+++ GitIntro/PrintMessage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace GitIntro
+{
+    public class PrintMessage
+    {
+
+        void showHello()
+        {
+            Console.WriteLine("Hello from Sabah ");
+
+        }
+
+
+    }
+}
